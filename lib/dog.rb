7682a4a3b190f994e4c0294652574da84d29d773
Mary Jack <maryjackinthebox@gmail.com>
2018-06-25 20:46:57 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -82,6 +82,7 @@ class Dog
   def update
     sql = "UPDATE dogs SET name = ?, breed = ? WHERE name = ?"
     DB[:conn].execute(sql, self.name, self.breed, self.name)
+    self.save
   end
 
  end	 
