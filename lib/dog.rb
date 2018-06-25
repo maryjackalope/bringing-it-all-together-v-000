7c3f844a89b01d0dfdf8037f77275e7fb3668a84
Mary Jack <maryjackinthebox@gmail.com>
2018-06-25 20:31:12 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,18 @@
-class Student
+class Dog
   
-  attr_accessor :name, :grade
+  attr_accessor :name, :breed
   attr_reader :id
   @@all= []
   
-  def initialize(name, grade, id = nil)
+  def initialize(name, breed, id = nil)
     @name = name
-    @grade = grade
+    @breed = breed
     @id = id
   end 
   
   def self.create_table
     sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS students(
+  CREATE TABLE IF NOT EXISTS dogs(
         id INTEGER PRIMARY KEY,
         name TEXT,
         grade TEXT
@@ -23,25 +23,25 @@ class Student
   end
   
   def self.drop_table
-    sql = "DROP TABLE IF EXISTS students"
+    sql = "DROP TABLE IF EXISTS dogs"
 
     DB[:conn].execute(sql)
   end
 
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade)
+      INSERT INTO dogs (name, breed)
       VALUES (?, ?)
     SQL
 
-    DB[:conn].execute(sql, self.name, self.grade)
+    DB[:conn].execute(sql, self.name, self.breed)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
-  def self.create(name:, grade:)
-    student = Student.new(name, grade)
-    student.save
-    student
+  def self.create(name:, breed:)
+    dog = Dog.new(name, grade)
+    dog.save
+    dog
   end
 
  end	 
